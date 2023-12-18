aa6e8b92112571046d347e071d40b62a4cccf563
LittiosiN <joaquinabelleira@gmail.com>
2023-12-18 17:39:48 -0300

    adding Message model with get info by user and add message complex logic

---
+++ server/src/models/Message.ts
@@ -0,0 +1,75 @@
+import { User } from "./User"
+
+export type Message = {
+  from: string
+  to: string
+  message: string
+  timestamp: string
+}
+
+export class MessageDB {
+  private static instance: MessageDB
+  private messages: Map<string, Map<string, Message[]>>
+
+  private constructor() {
+    this.messages = new Map<string, Map<string, Message[]>>()
+  }
+
+  public static getInstance(): MessageDB {
+    if (!MessageDB.instance) {
+      MessageDB.instance = new MessageDB()
+    }
+    return MessageDB.instance
+  }
+
+  getMessages (username:string):Map<string, Message[]> | undefined {
+    return this.messages.get(username)
+  }
+
+
+  addMessage (msg: Message):Message[] | undefined {
+    // no messages at all
+    if(!this.messages.get(msg.from)) {
+      const messagesMapSender = new Map<string,Message[]>()
+      const messagesMapReciever = new Map<string,Message[]>()
+      const msgArray = [].concat(msg)
+      messagesMapSender.set(msg.to,msgArray)
+      messagesMapReciever.set(msg.from, msgArray)
+      this.messages.set(msg.from, messagesMapSender)
+      // now we need to replicate for reciever
+      if (!this.messages.get(msg.to)){
+        this.messages.set(msg.to, messagesMapReciever)
+      } else {
+        const auxMap = this.messages.get(msg.to)
+        auxMap.set(msg.from, msgArray)
+      }
+      return this.messages.get(msg.from).get(msg.to)
+    }
+    const currentMapMsgs = this.messages.get(msg.from)
+    if (!currentMapMsgs.get(msg.to)) {
+      const msgArr = [].concat(msg)
+      currentMapMsgs.set(msg.to, msgArr)
+      //destinatary has no messages at all
+      if(!this.messages.get(msg.to)){
+        const messagesMapReciever = new Map<string,Message[]>()
+        messagesMapReciever.set(msg.from, msgArr)
+        this.messages.set(msg.to, messagesMapReciever)
+      } else {
+        const currentMapReciever = this.messages.get(msg.to)
+        // destinatary has no messages for sender
+        if (!currentMapReciever.get(msg.from)) {
+          currentMapReciever.set(msg.from, msgArr)
+        } else {
+          const currentArrReciever = currentMapReciever.get(msg.from)
+          currentMapReciever.set(msg.from, [...currentArrReciever, msg])
+        }
+      }
+      return this.messages.get(msg.from).get(msg.to)
+    }
+    const arrMsg = currentMapMsgs.get(msg.to)
+    const newArr = [...arrMsg, msg]
+    currentMapMsgs.set(msg.to, newArr)
+    this.messages.get(msg.to).set(msg.from, newArr)
+    return this.messages.get(msg.from).get(msg.to)
+  }
+}
