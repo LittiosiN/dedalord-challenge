e26aa958445b3318549c34b926a0995739883241
LittiosiN <joaquinabelleira@gmail.com>
2023-12-18 18:46:46 -0300

    adding message route to index and removing not used get users

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,5 +4,5 @@ import { register, getUsers, login } from '../controllers/auth.controller'
 export default (router:express.Router) => {
   router.post('/auth/register', register)
   router.post('/auth/login', login)
-  router.get('/auth', getUsers)
+  // router.get('/auth', getUsers)
 }
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import {HealthCheck} from '../handlers/HealthCheck'
 import authRoute from './auth'
+import messagesRoute from './messages'
 
 const router = express.Router()
 
 authRoute(router)
+messagesRoute(router)
 router.get('/', HealthCheck)
 
 export default router
